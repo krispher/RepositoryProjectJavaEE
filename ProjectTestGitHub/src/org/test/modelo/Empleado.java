0b94f7ef858c7396db7c7c6b878ebe5c61665b51
Cristhofer <Cristhofer@LAPTOP-SBN86649>
2021-08-06 13:05:54 -0500

    //se creo la clase entidad empleado

---
+++ ProjectTestGitHub/src/org/test/modelo/Empleado.java
@@ -0,0 +1,27 @@
+package org.test.modelo;
+
+public class Empleado {
+	
+	private int is;
+	private String nombre;
+	private String comentario;
+	public int getIs() {
+		return is;
+	}
+	public void setIs(int is) {
+		this.is = is;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getComentario() {
+		return comentario;
+	}
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	} 
+
+}
